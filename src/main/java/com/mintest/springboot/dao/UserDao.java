81248e5342b5294ea50d91c10f6bb96e84ce2a6e
minhyung9483 <minhyung9483@gmail.com>
2020-05-27 20:20:06 +0900

    mybatis로 mariadb 연동 v2.0

--- src/main/java/com/mintest/springboot/dao/UserDao.java
+++ src/main/java/com/mintest/springboot/dao/UserDao.java
@@ -6,19 +6,10 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.mintest.springboot.mapper.UserMapper;
 import com.mintest.springboot.vo.UserVo;
 
 @Repository
 public class UserDao {
-	/*
-	@Autowired
-	public UserMapper mapper;
-	
-	public List<UserVo> selectAll() {
-		return mapper.selectAll();
-	}
-	*/
 	@Autowired
 	SqlSession sqlSession;
 	
